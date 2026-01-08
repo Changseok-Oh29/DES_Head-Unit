25dda11f0a2cd347dcf3dd16565c13b642bc33f4
Changseok-Oh29 <changseo29@gmail.com>
2026-01-08 20:44:13 +0100

    vsomeip connection name modified

--- app/GearApp/src/VehicleControlClient.cpp
+++ app/GearApp/src/VehicleControlClient.cpp
@@ -35,7 +35,7 @@ void VehicleControlClient::connectToService()
     // Build proxy
     const std::string domain = "local";
     const std::string instance = "vehiclecontrol.VehicleControl";
-    const std::string connection = "client-sample";
+    const std::string connection = "GearApp";
     
     m_proxy = m_runtime->buildProxy<VehicleControlProxy>(domain, instance, connection);
     
